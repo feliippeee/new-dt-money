aa5acfe37b9e34a211df1a4992f4eb82f9fa613f
feliippeee <felipe.tecnosuporte@gmail.com>
2022-09-12 21:51:48 -0300

    Conexão com a API - Criando nova transação

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -4,6 +4,8 @@ import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton }
 import * as z from 'zod';
 import { Controller, useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useContext } from 'react';
+import { TransactionsContext } from '../../contexts/TransactionsContext';
 
 const newTransactionFormSchema = z.object({
     description: z.string(),
@@ -16,11 +18,13 @@ type NewTransactionFormInputs = z.infer<typeof newTransactionFormSchema>;
 
 
 export function NewTransactionModal() {
+    const { createTransaction } = useContext(TransactionsContext)
     const { 
         control,
         register, 
         handleSubmit,
-        formState: { isSubmitting} 
+        formState: { isSubmitting},
+        reset,
     } = useForm<NewTransactionFormInputs>({
         resolver: zodResolver(newTransactionFormSchema),
         defaultValues: {
@@ -30,8 +34,14 @@ export function NewTransactionModal() {
     })
 
     async function handleCreateNewTransaction(data: NewTransactionFormInputs){
-        await new Promise(resolve => setTimeout(resolve, 2000));
-        console.log(data);
+        const { description, price, category, type } = data;
+        await createTransaction({
+            description,
+            price,
+            category,
+            type,
+        })
+        reset();
     }
 
     return (
